9b2df4b114afe8d2376331716d2dff9b0b9e2291
Arti Mauryaa <artimaurya9721@gmail.com>
2024-03-10 14:51:10 +0530

    login with middleware

---
+++ middlewares/auth.mw.js
@@ -0,0 +1,69 @@
+/**
+ * create a middleware will check if the request body is proper and correct
+ */
+const user_model = require("../models/user.model")
+
+const verifySignUpBody = async (req, res, next)=>{
+
+    try{
+
+        //check for the name
+        if(!req.body.name){
+            return res.status(400).send({
+                message : "Failed ! Name was not provide in request body"
+            })
+        }
+
+        //check for the email
+        if(!req.body.email){
+            return res.status(400).send({
+                message : "Failed ! email was not provide in request body"
+            })
+        }
+
+        //check for the userId
+        if(!req.body.userId){
+            return res.status(400).send({
+                message : "Failed ! userId was not provide in request body"
+            })
+        }
+
+        //check if the user with the same userId is already present
+        const user = await user_model.findOne({userId : req.body.userId})
+
+        if(user){
+            return res.status(400).send({
+                message : "Failed ! user with same userId is already present"
+            }) 
+        }
+
+        next()
+
+    }catch(err){
+        console.log("Error while validating the request object", err)
+        res.status(500).send({
+            message : "Error while validating the request body"
+        })
+    }
+}
+
+const verifySignInBody = async (req, res, next)=>{
+
+    if(!req.body.userId){
+        return res.status(400).send({
+            message : "userId is not provided"
+        })
+    }
+
+    if(!req.body.password){
+        return res.status(400).send({
+            message : "password is not provided"
+        })
+    }
+    next()
+}
+
+module.exports = {
+    verifySignUpBody : verifySignUpBody,
+    verifySignInBody : verifySignInBody
+}
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,18 @@
 /**
- * POST 127.0.0.1:8080/ecomm/api/v1/auth/signup
+ * POST localhost:8080/ecomm/api/v1/auth/signup
  * 
  * I need to intercept this
  */
 const authController = require("../controllers/auth.controller")
+const authMW = require("../middlewares/auth.mw")
 
 module.exports = (app)=>{
-    app.post("/ecomm/api/v1/auth/signup", authController.signup)
+    app.post("/ecomm/api/v1/auth/signup",[authMW.verifySignUpBody], authController.signup)
+
+
+/**
+ * route for 
+ * POST localhost:8080/ecomm/api/v1/auth/signin
+ */
+app.post("/ecomm/api/v1/auth/signin",[authMW.verifySignInBody],authController.signin)
 }
